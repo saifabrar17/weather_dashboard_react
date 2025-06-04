615b8e97a07ae5affa8dad32e561a21f31a09391
Saif Abrar <saifabrar.1998@gmail.com>
2025-06-04 18:15:54 +0600

    components structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Header from './components/header/Header'
+import WeatherBoard from './components/weather/WeatherBoard'
 
 function App() {
 
   return (
-    <div>
+    <div className='grid place-items-center h-screen'>
      <Header />
+     <main>
+      <section>
+        <WeatherBoard />
+      </section>
+     </main>
     </div>
   )
 }
