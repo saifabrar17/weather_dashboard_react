cad12e8e3792b7ef89661f86af1bddebc1f4b18f
Saif Abrar <saifabrar.1998@gmail.com>
2025-06-17 18:11:25 +0600

    minor

--- src/components/weather/AddToFavourite.jsx
+++ src/components/weather/AddToFavourite.jsx
@@ -1,6 +1,12 @@
+import { useContext, useState } from 'react';
 import heart from '../../assets/heart.svg'
+import { FavouriteContext } from '../../context';
+
+
 export default function AddToFavourite() {
-  return (
+const {addTOFavourites, removeFromFavourites, favourites} = useContext(FavouriteContext);
+ const [isFavourite, toggleFavourite] = useState(false);
+return (
     <div className="md:col-span-2">
       <div className="flex items-center justify-end space-x-6">
         <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
